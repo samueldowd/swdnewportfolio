5713aebd18a0bdda9c3ed0882e754414910c4430
samueldowd <samuel_dowd@ny024x0fft0.home>
2013-11-21 18:25:31 -0500

    Work on new layout of schools and blogs

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+console.log("Hello from myscript.js");
+console.log("Hello again!");
+
+var name = prompt("What is your name?");
+
+alert("Hello " + name);
+
+name = "Nick";
+console.log("The user's name is " + name);
